5319290ea5a1a7bc50de4a148c16111837891450
KicaiBadai <adityamaula80@gmail.com>
2024-05-25 13:18:29 +0700

    Nambah Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Gabarito } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 
 const gabarito = Gabarito({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={gabarito.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
