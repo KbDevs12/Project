64063f68f9557b1266cd43b2f2127590084cc8fc
KbDevs12 <daniaku203@gmail.com>
2024-05-25 10:48:11 +0700

    update carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import EmblaCarousel from "@/components/Carousel/Embla";
+
 export default function Home() {
   return (
     <>
       <div>
         <p>test</p>
+        <div className="mt-8">
+          <EmblaCarousel />
+        </div>
       </div>
     </>
   );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,6 +8,7 @@ const Navbar = () => {
     profile: false,
     akademik: false,
     navOpen: false,
+    ftik: false,
   });
 
   const toggleNavMenu = () => {
@@ -15,19 +16,19 @@ const Navbar = () => {
   };
 
   return (
-    <header className="fixed top-0 left-0 right-0 bg-white shadow-md py-4 md:py-0 px-2 flex items-center justify-between z-50">
+    <header className="fixed top-0 left-0 right-0 bg-white shadow-md py-4 md:py-0 px-4 flex items-center justify-between z-50">
       <Link href="">
         <span className="font-bold text-2xl text-gray-500 cursor-pointer">
           logo
         </span>
       </Link>
 
-      {/* Hamburger menu untuk layar kecil */}
+      {/* menu untuk hp */}
       <div className="md:hidden absolute right-4 z-50">
         <Hamburger toggled={menuOpen.navOpen} toggle={toggleNavMenu} />
       </div>
 
-      {/* Menu navigasi untuk layar besar */}
+      {/* Menu navigasi untuk dekstop */}
       <nav className="hidden md:flex navbar">
         <ul className="list-none flex space-x-6">
           <li
@@ -36,34 +37,68 @@ const Navbar = () => {
             onMouseLeave={() => setMenuOpen({ ...menuOpen, profile: false })}
           >
             <Link href="">
-              <span className="text-lg py-5 px-4 text-gray-500 block hover:bg-gray-500 hover:text-white transition-all duration-300 cursor-pointer">
+              <span
+                className={`text-lg py-5 px-4 block hover:bg-gray-500 hover:text-white transition-all duration-300 cursor-pointer ${
+                  menuOpen.profile ? "text-blue-500" : "text-gray-500"
+                }`}
+              >
                 Profile
               </span>
             </Link>
             {menuOpen.profile && (
               <ul className="absolute left-0 w-48 bg-white">
-                <li className="w-full border-t border-gray-200">
+                <li
+                  className="w-full border-t border-gray-200 hover:bg-gray-500 hover:text-white transition-all duration-300"
+                  onMouseEnter={() => setMenuOpen({ ...menuOpen, ftik: true })}
+                  onMouseLeave={() => setMenuOpen({ ...menuOpen, ftik: false })}
+                >
+                  <Link href="">
+                    <span className="block py-2 px-4 cursor-pointer">
+                      Faculty Of Information Engineering
+                    </span>
+                  </Link>
+                  {menuOpen.ftik && (
+                    <ul className="absolute -right-48 top-0 w-48 bg-white text-black">
+                      <li className="w-full border-b border-gray-200 hover:bg-gray-500 hover:text-white transition-all duration-300">
+                        <Link href={""}>
+                          <span className="block py-2 px-4 cursor-pointer">
+                            Information Engineering
+                          </span>
+                        </Link>
+                      </li>
+                      <li className="w-full border-b border-gray-200 hover:bg-gray-500 hover:text-white transition-all duration-300">
+                        <Link href={""}>
+                          <span className="block py-2 px-4 cursor-pointer">
+                            Information System
+                          </span>
+                        </Link>
+                      </li>
+                      <li className="w-full border-b border-gray-200 hover:bg-gray-500 hover:text-white transition-all duration-300">
+                        <Link href={""}>
+                          <span className="block py-2 px-4 cursor-pointer">
+                            Film, Television & Media Studies
+                          </span>
+                        </Link>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+
+                <li className="w-full border-t border-gray-200 hover:bg-gray-500 hover:text-white transition-all duration-300">
                   <Link href="">
                     <span className="block py-2 px-4 cursor-pointer">
                       Fakultas FTIK
                     </span>
                   </Link>
                 </li>
-                <li className="w-full border-t border-gray-200">
-                  <Link href="">
-                    <span className="block py-2 px-4 cursor-pointer">
-                      Fakultas FTIK
-                    </span>
-                  </Link>
-                </li>
-                <li className="w-full border-t border-gray-200">
+                <li className="w-full border-t border-gray-200 hover:bg-gray-500 hover:text-white transition-all duration-300">
                   <Link href="">
                     <span className="block py-2 px-4 cursor-pointer">
                       Prodik SI
                     </span>
                   </Link>
                 </li>
-                <li className="w-full border-t border-gray-200">
+                <li className="w-full border-t border-gray-200 hover:bg-gray-500 hover:text-white transition-all duration-300">
                   <Link href="">
                     <span className="block py-2 px-4 cursor-pointer">
                       Prodi Kajian Film, TV, dan Media
@@ -92,14 +127,14 @@ const Navbar = () => {
             </Link>
             {menuOpen.akademik && (
               <ul className="absolute left-0 w-48 bg-white">
-                <li className="w-full border-t border-gray-200">
+                <li className="w-full border-t border-gray-200 hover:bg-gray-500 hover:text-white transition-all duration-300">
                   <Link href="">
                     <span className="block py-2 px-4 cursor-pointer">
                       Kurikulum
                     </span>
                   </Link>
                 </li>
-                <li className="w-full border-t border-gray-200">
+                <li className="w-full border-t border-gray-200 hover:bg-gray-500 hover:text-white transition-all duration-300">
                   <Link href="">
                     <span className="block py-2 px-4 cursor-pointer">
                       Panduan Akademik
